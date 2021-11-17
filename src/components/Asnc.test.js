08795c3bf04c872d4e3ca62213454b99b1c8e060
rkrohne1991 <rafal.krohne@hotmail.co.uk>
2021-11-17 07:16:57 +0000

    Working With Mocks

--- src/components/Asnc.test.js
+++ src/components/Asnc.test.js
@@ -4,6 +4,10 @@ import Async from "./Async";
 describe("Async component", () => {
   test("renders posts if request succeeds", async () => {
     // Arrange
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "p1", title: "First post" }],
+    });
     render(<Async />);
 
     // Act
